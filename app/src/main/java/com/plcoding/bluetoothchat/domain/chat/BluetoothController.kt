982f0861d626099f78c6e93c960f8a50f49f0ae2
lovedivineonwulata <ldonwulata@gmail.com>
2023-10-30 13:34:27 -0400

    testing

--- app/src/main/java/com/plcoding/bluetoothchat/domain/chat/BluetoothController.kt
+++ app/src/main/java/com/plcoding/bluetoothchat/domain/chat/BluetoothController.kt
@@ -1,6 +1,5 @@
 package com.plcoding.bluetoothchat.domain.chat
 
-import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.StateFlow
 
 interface BluetoothController {
@@ -9,10 +8,10 @@ interface BluetoothController {
 
     fun startDiscovery()
     fun stopDiscovery()
-
+/*
     fun startBluetoothServer(): Flow<ConnectionResult>
     fun connectToBluetooth(device: BluetoothDevice): Flow<ConnectionResult>
-
+*/
     fun playScreen()
 
     fun release()
