059ac334b732805ead7f06464ef9eefa4c0c7ca8
CBussom <52250345+CBussom@users.noreply.github.com>
2023-11-16 15:00:57 -0500

    Delete app directory

--- app/src/main/java/Tests/ExampleTest.kt
+++
@@ -1,9 +0,0 @@
-package Tests
-import org.junit.Test
-import org.junit.Assert.*
-class ExampleTest {
-    @Test
-    fun addition(){
-        assertEquals(1+2, 3)
-    }
-}
--- app/src/main/java/com/plcoding/bluetoothchat/data/chat/GameController.kt
+++
@@ -1,5 +0,0 @@
-package com.plcoding.bluetoothchat.data.chat
-
-class GameController {
-
-}
